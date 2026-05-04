a2bb7a0b3dd121eee3638f8949760d877a1acc34
yandapallidivyareddy-hub <yandapallidivyareddy@gmail.com>
2026-05-04 18:39:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 import streamlit as st
 import numpy as np
-from tensorflow.keras.models import load_model
+import joblib
 
 # Load model
-model = load_model('fraud_model.keras', compile=False)
+model = joblib.load("fraud_model.pkl")
 
 st.title("Credit Card Fraud Detection")
 
-st.write("Enter transaction values separated by commas")
-
-input_data = st.text_area("Input Data")
+input_data = st.text_area("Enter transaction values separated by commas")
 
 if st.button("Predict"):
     try:
         data = np.array([float(x) for x in input_data.split(',')]).reshape(1, -1)
+
         prediction = model.predict(data)
-        prob = prediction[0][0]
 
-        st.write(f"Fraud Probability: {prob:.4f}")
-
-        if prob > 0.5:
+        if prediction[0] == 1:
             st.error("Fraud Transaction Detected")
         else:
             st.success("Genuine Transaction")
